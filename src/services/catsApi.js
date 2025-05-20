cb5950f9ab48773eb171947c5d41f54361b62687
joseLaptop <josenarvaezcariaga@gmail.com>
2025-05-20 16:39:24 -0500

    feat: se actualizaron las pages y componentes para que todo funcione con el back Se agregó y editaron los llamados de dogsApi y catsApi

--- src/services/catsApi.js
+++ src/services/catsApi.js
@@ -6,21 +6,29 @@ import CatModel from "../models/catModel";
 // 1. Obtenemos la clave de API desde las variables de entorno
 const CAT_API_KEY = import.meta.env.VITE_CAT_API_KEY || 'your-default-key';
 // 2. Definimos la URL base de la API
-const CAT_API_URL = 'https://api.thecatapi.com/v1';
+const CAT_API_URL = 'https://petplatformback-duateac4dbh3bahj.canadacentral-01.azurewebsites.net/api/cats/breeds';
 
 // 3. Función para obtener todas las razas de gatos
 export const getAllCatBreeds = async () => {
 
-    const url = `${CAT_API_URL}/breeds`;
+    const url = `${CAT_API_URL}`;
     const mensajeError = "Error fetching cat breed";
     
     const response = await peticionesfetch(url,CAT_API_KEY,mensajeError);
 
-    // Assuming the API returns a JSON object with a 'data' property containing the array of breeds
+    
     console.log(response);
-    const catBreeds = response.map(breedData => new CatModel(breedData));
+    const catBreeds = response.data.map(breedData => new CatModel(breedData));
     return catBreeds;
 
+};
+
+export const getCatById = async (breedId) => {
+
+    const url = `${CAT_API_URL}/${breedId}`;
+    const mensajeError = 'Error fetching cat images';
+    return peticionesfetch(url,CAT_API_KEY,mensajeError);
+    
 };
 
 // 9. Función para obtener imágenes de una raza específica
