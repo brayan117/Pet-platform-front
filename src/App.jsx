7b64cbdb174bf380b2c107ab1ba4a1d0acd5fbfa
yeiler2111 <yeiler2209@gmail.com>
2025-05-25 19:46:01 -0500

    add refactory home

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,54 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import CatsPage from "./pages/CatsPage";
-import DogsPage from "./pages/DogsPage";
-import CatDetailPage from "./pages/CatDetailPage";
-import DogDetailPage from "./pages/DogDetailPage";
-import UserBar from "./components/UserBar/UserBar";
 import CurrentUserInfo from "./components/UserBar/CurrentUserInfo";
-import FavoritesPage from "./pages/FavoritesPage";
+import UserBar from "./components/UserBar/UserBar";
 import BreedComparatorPage from "./pages/BreedComparatorPage"; // Import the new page
+import CatDetailPage from "./pages/CatDetailPage";
+import CatsPage from "./pages/CatsPage";
+import DogDetailPage from "./pages/DogDetailPage";
+import DogsPage from "./pages/DogsPage";
+import FavoritesPage from "./pages/FavoritesPage";
 
-const Home = () => <div className="p-4">Home Page</div>
+const Home = () => (
+  <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <img
+      className="w-1/3 max-w-xs rounded-2xl shadow-xl transition-transform duration-500 hover:scale-105"
+      src="src/assets/logo.jpg"
+      alt="logo"
+    />
+    <h1 className="mt-6 text-3xl font-bold text-gray-800">
+      Bienvenido a Nuestra Plataforma: {<CurrentUserInfo />}
+    </h1>
+    <p className="mt-2 text-lg text-gray-600">
+      Explora las funcionalidades que hemos preparado para ti.
+    </p>
+  </div>
+);
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Header />
-                <UserBar />
-                <CurrentUserInfo />
-                <main className="container mx-auto mt-4">
-                    <Routes>
-                        <Route path="/" element={<Home/>} />
-                        <Route path="/gatos" element={<CatsPage/>}/>
-                        <Route path="/gatos/:breedId" element={<CatDetailPage />} />
-                        <Route path="/perros" element={<DogsPage/>}/>
-                        <Route path="/perros/:breedId" element={<DogDetailPage />} />
-                        <Route path="favoritos" element={<FavoritesPage />} />
-                        <Route path="/matcher" element={<BreedComparatorPage/>}/>
-                        <Route path="/comparador" element={<BreedComparatorPage />} /> {/* Add the new route */}
-                    </Routes>
-                </main>
-            </div>
-        </Router>
-    )
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <UserBar />
+
+        <main className="w-full">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/gatos" element={<CatsPage />} />
+            <Route path="/gatos/:breedId" element={<CatDetailPage />} />
+            <Route path="/perros" element={<DogsPage />} />
+            <Route path="/perros/:breedId" element={<DogDetailPage />} />
+            <Route path="favoritos" element={<FavoritesPage />} />
+            <Route path="/matcher" element={<BreedComparatorPage />} />
+            <Route path="/comparador" element={<BreedComparatorPage />} />{" "}
+            {/* Add the new route */}
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
